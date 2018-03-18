7a50977d56e43f7e2efb307eb5ac59b9b73b61f4
Shijia Hu <shu27@binghamton.edu>
2018-03-17 20:56:05 -0400

    Add files via upload

---
+++ confclient.c
@@ -0,0 +1,116 @@
+/*--------------------------------------------------------------------*/
+/* conference client */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#include <errno.h>
+
+#include <stdlib.h>
+
+#define MAXMSGLEN  1024
+
+extern char * recvtext(int sd);
+extern int sendtext(int sd, char *msg);
+
+extern int hooktoserver(char *servhost, ushort servport);
+/*--------------------------------------------------------------------*/
+
+/*--------------------------------------------------------------------*/
+int main(int argc, char *argv[]) {
+	int sock;
+
+	fd_set rfds;
+	int retval;
+
+	/* check usage */
+	if (argc != 3) {
+		fprintf(stderr, "usage : %s <servhost> <servport>\n", argv[0]);
+		exit(1);
+	}
+
+
+
+	
+	/* get hooked on to the server */
+	sock = hooktoserver(argv[1], atoi(argv[2]));
+	if (sock == -1) {
+		perror("Error: ");
+		exit(1);
+	}
+
+
+   //struct timeval tv;
+//  fd_set readfds;
+ //tv.tv_sec = 2;
+  //tv.tv_usec = 500000;
+//  FD_ZERO(&readfds);
+//  FD_SET(STDIN, &readfds);
+// select(STDIN+1, &readfds, NULL, NULL, &tv);
+// if (FD_ISSET(STDIN, &readfds))
+//         printf("A key was pressed!\n");
+//  else printf("Timed out.\n");
+
+	FD_ZERO(&rfds);
+
+	FD_SET(0, &rfds);
+
+	FD_SET(sock, &rfds);
+
+	retval = sock;
+	/* keep talking */
+
+	while (1) {
+	
+		/*
+		 FILL HERE
+		 use select() to watch simultaneously for
+		 inputs from user and messages from server
+		 */
+	FD_ZERO(&rfds);
+
+	FD_SET(0, &rfds);
+
+	FD_SET(sock, &rfds);
+
+	retval = sock;
+		if (select(retval + 1, &rfds, NULL, NULL, NULL) < 0 ){
+			perror("Error: select error");
+			exit(1);
+		}
+
+		if (FD_ISSET(sock, &rfds)/* FILL HERE: message from server? */) {
+			char *msg;
+			msg = recvtext(sock);
+			if (!msg) {
+				/* server killed, exit */
+				fprintf(stderr, "error: server died\n");
+				exit(1);
+			}
+
+			/* display the message */
+			printf(">>> %s", msg);
+			/* free the message */
+			free(msg);
+		}
+
+		if (FD_ISSET(0, &rfds)/* FILL HERE: input from keyboard? */) {
+			char msg[MAXMSGLEN];
+			if (!fgets(msg, MAXMSGLEN, stdin))
+				exit(0);
+			//printf("i press: %s", msg);
+			sendtext(sock, msg);
+		}
+
+		printf(">");
+		fflush(stdout);
+	}
+	return 0;
+}
+/*--------------------------------------------------------------------*/
